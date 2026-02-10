db4cd4eda54e8432e5bca6c8b95d0da3a1b7b6a5
Nittishna <75472177+Xyandraa9@users.noreply.github.com>
2026-02-10 15:02:27 +0000

    added optimised solution

--- 1TwoSumProb.java
+++ 1TwoSumProb.java
@@ -1,13 +1,31 @@
+//BruteForce
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for(int i =0; i<nums.length;i++){
+//             int complement = target-nums[i];
+//             for(int j = i+1; j<nums.length;j++){
+//                 if(nums[j] == complement){
+//                     return new int[]{i,j};
+//                 }
+                
+//             }
+//         }
+//         return new int[]{-1,-1};
+//     }
+// }
+
+// O(n) optimised using hashmaps
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i =0; i<nums.length;i++){
             int complement = target-nums[i];
-            for(int j = i+1; j<nums.length;j++){
-                if(nums[j] == complement){
-                    return new int[]{i,j};
-                }
-                
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
+            map.put(nums[i],i);
         }
         return new int[]{-1,-1};
     }
